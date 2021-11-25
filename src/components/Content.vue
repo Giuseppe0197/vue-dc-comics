ef7c49051dd61f6eab5602d7ba43d5fa21956b1f
Giuseppe0197 <faro.g@hotmail.it>
2021-11-25 14:59:08 +0100

    parte fumetti stmpati con microcomponente

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,33 +1,185 @@
 <template>
-  <section>
+    <section>
 
-      <div>
-          Content Goes Here
+      <div id="teenTitansDc">
+          
       </div>
 
-  </section>
+      <button id="series">Current Series</button>
+
+      <div id="thumbsDc">
+
+        <div id="thumbsContainer">
+
+        <!-- inserimento componente da creare con un ciclo v-for e stampare tutte le miniature dei fumetti -->
+        <!-- <div class="thumbsComics">
+            <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="">
+            <p>Action Comics</p>
+        </div> -->
+        <thumbComp v-for="comics, i in thumbsComics" :key="i" :details="comics"/>
+
+        </div>
+
+        <div class="buttonLoad">
+
+            <button id="load">Load More</button>
+
+        </div>
+
+
+      </div>
+
+    </section>
 </template>
 
 <script>
+import thumbComp from '@/components/thumbComp.vue';
+
 export default {
   name: 'Content',
-  
+  components: {
+      thumbComp
+  },
+
+  data() {
+      return {
+
+          thumbsComics: [
+            {
+                "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX",
+                "price": "$19.99",
+                "series": "Action Comics",
+                "type": "comic book"
+            },
+            {
+                "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/AV1976_01_300-001_HD_5f738f6e39ddd7.18205602.jpg?itok=VgdYdJ01",
+                "price": "$3.99",
+                "series": "American Vampire 1976",
+                "type": "comic book"
+            },
+            {
+                "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2018/08/AQMv4_UW_153-001_HD_5b7efa1745a9b8.08589135.jpg?itok=HPTtnrsb",
+                "price": "$16.99",
+                "series": "Aquaman",
+                "type": "graphic novel"
+            },
+            {
+                "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/06/batgirl_v5_1_5b23e1f1124941.50950783.jpg?itok=VnrJah76",
+                "price": "$2.99",
+                "series": "Batgirl",
+                "type": "comic book"
+            },
+            {
+                "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/BM_56_300-001_HD_5ba137a85c3bf3.33122291.jpg?itok=3FHJQYJZ",
+                "price": "$3.99",
+                "series": "Batman",
+                "type": "comic book"
+            },
+            {
+                "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/BM_56_300-001_HD_5ba137a85c3bf3.33122291.jpg?itok=3FHJQYJZ",
+                "price": "$2.99",
+                "series": "Batman Beyond",
+                "type": "comic book"
+            },
+            {
+                "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2019/08/BMSM_5d4361b7116261.74371456.jpg?itok=kEDR2OL8",
+                "price": "$3.99",
+                "series": "Batman/Superman",
+                "type": "comic book"
+            },
+            {
+                "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMSMANN_01_300-001_HD_5f5ff17fa1d665.74704970.jpg?itok=azz5sfGk",
+                "price": "$4.99",
+                "series": "Batman/Superman Annual",
+                "type": "comic book"
+            },
+            {
+                "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMTJWZ_01_300-001_HD_5f5ff2307dcb37.34652945.jpg?itok=VswVjLR8",
+                "price": "$5.99",
+                "series": "Batman: The Joker War Zone",
+                "type": "comic book"
+            },
+            {
+                "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/08/cover-v2_5f40314645a734.37285065.png?itok=ImNM2QBY",
+                "price": "$6.99",
+                "series": "Batman: Three Jokers",
+                "type": "comic book"
+            },
+            {
+                "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/10/BMWK_HQ_01_300-001_HD_5f7cb4945e13f6.89501032.jpg?itok=sVwALbUX",
+                "price": "$4.99",
+                "series": "Batman: White Knight Presents: Harley Quinn",
+                "type": "comic book"
+            },
+            {
+                "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2019/04/CTWv1_CC_144-001_HD_5ca5299a751963.53054221.jpg?itok=ooPaoLDq",
+                "price": "$16.99",
+                "series": "Catwoman",
+                "type": "graphic novel"
+            }
+            ]
+
+      }
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-    section {
-        height: 140px;
-        background-color: #1C1C1C;
+  
+  section {
+      position: relative;
 
-        div {
-            width: 80%;
-            margin: auto;
-            line-height: 120px;
-            color: white;
-            font-size: 30px;
+      #teenTitansDc {
+          height: 400px;
+          background-image: url("../assets/img/jumbotron.jpg");
+          background-size: cover;
+          
+      }
+
+      #series {
+          position: absolute;
+          top: 380px;
+          left: 10%;
+          border: none;
+          font-size: 25px;
+          text-transform: uppercase;
+          background-color: #0282F9;
+          color: white;
+          padding: 5px 10px;
+          cursor:pointer
+      }
+
+      #thumbsDc {
+          background-color: #1C1C1C;
+          min-height: 400px;
+          padding-top: 50px;
+          
+
+          #thumbsContainer {
+              width: 80%;
+              margin: auto;
+              display: flex;
+              flex-wrap: wrap;
+              justify-content: space-between;
+
+              
+          }
+          .buttonLoad {
+              text-align: center;
+          }
+
+          #load {
+          border: none;
+          font-size: 25px;
+          text-transform: uppercase;
+          background-color: #0282F9;
+          color: white;
+          padding: 5px 10px;
+          margin-bottom: 20px;
+          cursor: pointer;
         }
     }
+  }
     
 </style>
---
+++ src/components/thumbComp.vue
@@ -0,0 +1,46 @@
+<template>
+  
+      <div class="thumbsComics">
+            <img :src="details.thumb" alt="">
+            <p>{{details.series}}</p>
+            <p class="typeCom">{{details.type}}</p>
+            <p class="priceCom">{{details.price}}</p>
+
+        </div>
+  
+</template>
+
+<script>
+export default {
+  name: 'thumbComp',
+  props: {
+      details: Object
+  }
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+    .thumbsComics {
+        width: calc(100% / 6);
+        
+
+        img {
+            width: 100%;
+            object-fit: none;
+            object-position: top;
+            height: 70%;
+        }
+
+        p {
+            color: white;
+
+            &.typeCom, &.priceCom {
+                margin-top: 5px;
+            }
+
+        }
+    }
+    
+</style>
